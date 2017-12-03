4ed06b5e9d1b8f17c098c0a517c6a888433b498c
CarlosVecina <32160447+CarlosVecina@users.noreply.github.com>
2017-12-03 20:32:08 +0100

    View www.carlosvecina.es/sample-generation

--- Multivariate normal distribution & fitting models/sample-generation.R
+++ Multivariate normal distribution & fitting models/sample-generation.R
@@ -1,15 +1,13 @@
-library(shiny)
 library(shinydashboard)
+library(shiny)
 library(ggplot2)
 library(MASS)
+library(plotly)
 library(shinythemes)
 
-
-
-ui <- dashboardPage(
-    
-    
-    
+ui <- 
+  
+  dashboardPage(
     dashboardHeader(title = "Sample generation",
                     # Menu de mensajes.
                     dropdownMenu(type = "messages", badgeStatus = "success",
@@ -25,97 +23,108 @@ ui <- dashboardPage(
                                              "carlos.vecina@cunef.edu",
                                              time = "Now"
                                  )
-                    )
-    )
-    ,
+                    )),
     dashboardSidebar(bootstrapPage(
-        
-        #tags$style(".logo {background-color: red !important}") # Cambiar color del logo del encabezado.
-        numericInput("samplesize", label = "Sample size", value=100),  
-        checkboxGroupInput("check", label = NULL, 
-                           choices = list("Divide sample in training and test" = 1)),
-        conditionalPanel(
-            condition = "input.check == '1'",
-            numericInput("procentajet", label= "Set training %", value=50, min=1, max=100)),
-        numericInput("correlation", label = "Pearson correlation", value= 0.5, min=-1, max=1, step=0.1),
-        numericInput("semilla", label= "Set seed", value=2017, min=1),
-        actionButton("generate", label = "Generate", icon= icon("refresh")),
-        numericInput("orden", label= "Polynomial order", value=1, min=1, max=10),
-        actionButton("pregresion", label = "Plot model", icon= icon("area-chart")),
-        theme = shinytheme("united")
-        
-        
+      numericInput(inputId = "samplesize",label = "Sample size",  value = 100),
+      numericInput(inputId = "correlation",label = "Correlation", value= 0.5, step =0.1, min=-1, max=1),
+      actionButton('generate','Generate',icon('refresh')),
+      checkboxInput("checkbox", label = "Training / Test samples", value = F),
+      conditionalPanel(
+        condition = "input.checkbox=='1'",
+        textInput(inputId = "porcentajet",label = "Porcentaje de training",value = .5),
+        actionButton('genetrain','Train/Test',icon('refresh'))
+      ),
+      numericInput(inputId = "order",label = "Polynomic Order", value=NA, min=1, max=10),
+      actionButton('genelm','Linear Model',icon('area-chart')),
+      theme = shinytheme("united")
+      
     )),
     dashboardBody(
-        # Insertamos HTML para mejorar el formato
         tags$div(    
             HTML("<CENTER><H4>Welcome to my shiny app! Here you can generate bivariate samples from the specified multivariate normal distribution. </H4></CENTER><br/>")
         ),
         tags$div(    
             HTML("<CENTER><H5>Press the buton 'Generate' to plot it. <br/>
                  Also you can fit a regresion model for the sample. Choose the polynomial order and press 'Plot model'.</H5></CENTER><br/>")
-        ),
-        
-        textOutput("bienvenida"),
-        plotOutput("grafica", click = "puntitobomba"),
-        tags$div(    
-            HTML("<CENTER><H5> * Note that if you set a small sample (e.g. 10) and try to fit a high regression order (ofc with correlation != 1), you will show clearly the overfitting problem.</H5></CENTER><br/>")
-        )
+            ),
+      plotlyOutput('grafica'),
+      verbatimTextOutput('summary'),
+      tags$div(    
+          HTML("<CENTER><H5> * Note that if you set a small sample (e.g. 10) and try to fit a high regression order (ofc with correlation != 1), you will show clearly the overfitting problem.</H5></CENTER><br/>")
+      )
     )
-    
-)
+  )
+  
 
 
-
-
-server <- function(input, output) {
+server <- function(input, output, session) {
+  mu <- c(0,0)
+  sigma <- matrix(c(1,0.5,0.5,1),2,2,byrow =T)
+  Data <- reactiveValues(sample = as.data.frame(mvrnorm(100,mu,sigma)))
+  Plot1 <- reactiveValues(main=NULL, layer1=NULL, layer2=NULL)
+  
+  
+  observeEvent(input$generate,{
+    mu <- c(0,0)
+    a<- as.numeric(input$correlation)
+    sigma <- matrix(c(1,a,a,1),2,2,byrow =T)
+    n <- as.numeric(input$samplesize)    
+    Data$sample <-as.data.frame(mvrnorm(n,mu,sigma))
     
+  })
+  
+  observeEvent(input$checkbox | input$genetrain | input$generate,{
+    prc <- as.numeric(input$porcentajet)
+    prcfinal<- sample(nrow(Data$sample),prc*nrow(Data$sample),replace = F)
+    Data$sample[prcfinal,3]<-'train'
+    Data$sample[-prcfinal,3]<-'test'
+    Data$sample[,3]<- as.factor(Data$sample[,3])
     
+  })
+  
+  observeEvent(input$genelm| input$checkbox | input$genetrain ,{
+    grado <- as.numeric(input$order)
+    d1 <- Data$sample[Data$sample[,3]=='train',]
+    d2 <- Data$sample[Data$sample[,3]=='test',]
+    Plot1$layer1 <- geom_smooth(method = 'lm', se = FALSE,
+                                formula = y ~ poly(x,grado),colour=grado+1,
+                                show.legend = T)
     
+    if(input$checkbox & !is.na(grado)){
+      Plot1$layer1 <- geom_smooth(data = d1,aes(x=V1,y=V2),method = 'lm', se = FALSE,
+                                  formula = y ~ poly(x,grado),colour=grado+1,
+                                  show.legend = T)
+      Plot1$layer2 <- geom_smooth(data = d2,aes(x=V1,y=V2),method = 'lm', se = FALSE,
+                                  formula = y ~ poly(x,grado),colour=grado+2,
+                                  show.legend = T)
+    }else{
+      Plot1$layer1
+    }
+  })
+  
+  observeEvent(input$generate,{
+  output$grafica <- renderPlotly({
     
-    misDatos<- reactiveValues(muestra=NULL) # Dentro de esta lista reactive creamos una variable y la iniciamos. 
+    Plot1$main <- ggplot(Data$sample,aes(x=V1,y=V2))+
+      geom_point() + Plot1$layer1 + xlab('X') + ylab('Y') 
+    if(input$checkbox){
+      plotly_graf <- ggplotly(Plot1$main +  geom_point(aes(colour=V3)) + Plot1$layer2 )
+      plotly_graf
+      
+    }else{
+      plotly_graf <- ggplotly(Plot1$main)
+      plotly_graf
+    }
+    
+  })
  
-    observeEvent(input$generate, {
-        
-        
-        isolate(set.seed(input$semilla))
-        Sigma <- isolate(matrix(c(1,input$correlation,input$correlation,1),2,2)) # Isolate para aislar los inputs. Creamos una matriz de correlación con la corr introducida por el usuario.
-        misDatos$muestra<-as.data.frame(mvrnorm(isolate(input$samplesize),c(0,0),Sigma)) # Generación de muestra multivariante. Con la matriz de correlación indicamos queson dos variables.
-        
-        output$grafica<- renderPlot({   # Ploteo la grafica que cambia con el reactive.
-            
-            
-            ggplot(misDatos$muestra) + geom_point(aes(x= V1, y = V2)) +
-                labs(x = "Variable 1",y = "Variable 2") #+ geom_smooth(method="lm", aes(x =misDatos$muestra$V1, y = misDatos$muestra$V2), formula=  y ~ poly(x, degree = 4),  se=FALSE, linetype = 1)
-            
-        })
-        
-    })  
-    
-    observe({   
-        input$puntitobomba  # Quiero que se ejecute cada vez que se haga click en el grafico.
-        isolate({
-            misDatos$x<-c(misDatos$x,input$puntitobomba$x)  # Guardo la cordenada del eje x en el reactive.
-            misDatos$y<-c(misDatos$y, input$puntitobomba$y) # Guardo la cordenada del eje y en el reactive.
-            misDatos$muestra<- rbind(misDatos$muestra, cbind(input$puntitobomba$x, input$puntitobomba$y))    # Los introduzco en "muestra".
-            
-        })
-    })  
-    
-    
-    # Fitar el modelo del orden indicado.
-    observeEvent(input$pregresion, {
-        
-        orden<- input$orden
-        output$grafica <- renderPlot({
-            
-            ggplot(misDatos$muestra) + geom_point(aes(x = V1, y = V2)) +xlab("Var 1")+ ylab("Var 2") + geom_smooth(method="lm", aes(x = V1, y = V2), formula=  y ~ poly(x, degree = orden),  se=FALSE, linetype = 1)
-        
-            })
-        
-    })
-    
+ 
+  output$summary <- renderText({
+    paste0('La correlación de Pearson es: ',cor(Data$sample[,1:2])[1,2])
+  })
+  })
 }
 
 
-shinyApp(ui,server)
+shinyApp(ui, server)
+
